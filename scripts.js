69445e9a8559bc8a9d44b00d80fd8a5a9a9d3a32
syaibaadi <syaibaadi03@gmail.com>
2022-10-12 11:52:46 +0700

    JS Day 2

--- scripts.js
+++ scripts.js
@@ -7,25 +7,29 @@ function bookPurchase() {
     const tax = 10 / 100;
     let cek = confirm("Apakah Anda akan membeli buku?");
     input = prompt('Masukan Jumlah Buku: '+stok);
-    if(input<=10){
-        for(input; input <= stok; input-1){
-            if(input <= stok){
-                total = priceBook * input;
-                total = total - total * disc;
-                total = total + total * tax;
-                console.log('Kamu membeli '+input+' Buku');
-                console.log('Total Harga : '+total+'');
-                stok = stok - input;
-                input = prompt('Masukan Jumlah Buku: '+stok);
-                if(input > stok){
-                    console.log("Habis Bro")
+    if(cek == true){
+        if(input<=10){
+            for(input; input <= stok; input-1){
+                if(input <= stok){
+                    total = priceBook * input;
+                    total = total - total * disc;
+                    total = total + total * tax;
+                    console.log('Kamu membeli '+input+' Buku');
+                    console.log('Total Harga : '+total+'');
+                    stok = stok - input;
+                    input = prompt('Masukan Jumlah Buku: '+stok);
+                    if(input > stok){
+                        console.log("Habis Bro")
+                    }
+                }else{
+                    console.log("Kamu melebihi stok");
                 }
-            }else{
-                console.log("Kamu melebihi stok");
             }
+        }else{ 
+            console.log("Pesanan Melebihi Stok");
         }
-    }else{ 
-        console.log("Pesanan Melebihi Stok");
+    }else{
+        console.log("Terima Kasih");
     }
 }
 
