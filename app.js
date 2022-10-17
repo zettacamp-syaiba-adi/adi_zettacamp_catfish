fc48b46e099330e498b89538819dc84e68dde3fe
syaibaadi <syaibaadi03@gmail.com>
2022-10-17 16:07:48 +0700

    TS Day 2

---
+++ app.js
@@ -0,0 +1,18 @@
+var casesatu = [1, 'data', 3, 'result'];
+var hasilcasesatu = '';
+var casedua = ['Bejo', 'has', '4', 'sport', 'car'];
+var hasilcasedua = '';
+function casepertama() {
+    for (var i = 0; i < casesatu.length; i++) {
+        hasilcasesatu += casesatu[i];
+        hasilcasesatu += ' ';
+    }
+    return hasilcasesatu;
+}
+console.log(casepertama());
+console.log(typeof casepertama());
+function casekedua() {
+    return (hasilcasedua = casedua.join(' '));
+}
+console.log(casekedua());
+console.log(typeof casekedua());
---
+++ app.ts
@@ -0,0 +1,22 @@
+let casesatu: Array<string | number> = [1, 'data', 3, 'result'];
+let hasilcasesatu:string = '';
+
+let casedua: Array<string> = ['Bejo', 'has', '4', 'sport', 'car'];
+let hasilcasedua:string = '';
+
+function casepertama(): string{
+    for(let i:number = 0 ; i < casesatu.length; i++){
+        hasilcasesatu += casesatu[i];
+        hasilcasesatu += ' ';
+    }
+    return hasilcasesatu;
+}
+console.log(casepertama());
+console.log(typeof casepertama());
+
+function casekedua():string {
+    return (hasilcasedua = casedua.join(' '));
+}
+console.log(casekedua());
+console.log(typeof casekedua());
+
